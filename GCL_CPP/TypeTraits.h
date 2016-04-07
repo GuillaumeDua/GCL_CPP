ea4d268ca47ccb0e93eb1549d2190630aa876424
GuillaumeDua <guillaume.dua@gmail.com>
2016-04-07 22:49:19 +0200

    Adding new object : GCL::TypeTraint::Any

--- GCL_CPP/TypeTraits.h
+++ GCL_CPP/TypeTraits.h
@@ -10,22 +10,36 @@ namespace GCL
 		{
 			using _Type = T;
 			static const int value;
-
 		};
 		template <typename T>
 		const int TypeToUniqueId<T>::value = reinterpret_cast<int>(&TypeToUniqueId<T>::value);
 
-		template <int TypeId>
-		struct IntToType
+		struct Any
 		{
-			// [Todo]
+			template <typename T>
+			explicit Any(T & var)
+				: _ptr(&var)
+				, _typeUniqueId(TypeToUniqueId<T>::value)
+			{}
+
+			template <typename T>
+			inline T & GetAs(void)
+			{
+				if (TypeToUniqueId<T>::value != _typeUniqueId)
+					std::cerr << "[Warning] : Bad type requested : [" << _typeUniqueId << "] to [" << TypeToUniqueId<T>::value << ']' << std::endl;
+				return *reinterpret_cast<T*>(_ptr);
+			}
+
+		private:
+			void *      _ptr;
+			const int   _typeUniqueId;
 		};
 
 		struct Test
 		{
 			struct A{}; struct B{};
 
-			static bool Proceed(void) _noexcept
+			static bool Proceed(void)
 			{
 				return
 				GCL::TypeTrait::TypeToUniqueId<A>::value == GCL::TypeTrait::TypeToUniqueId<A>::value
