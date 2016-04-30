b7ed6c1ec9a340e17cde57d983fe5f9f63ac5a2c
GuillaumeDua <guillaume.dua@gmail.com>
2016-04-30 22:23:46 +0200

    Improving test-component system

--- GCL_CPP/Main.cpp
+++ GCL_CPP/Main.cpp
@@ -14,14 +14,22 @@
 # include <thread>
 # include <iomanip>
 # include <typeinfo>
+# include <sstream>
 
 template <typename T_ComponentTest>
 bool	TestComponent(void)
 {
+	const std::string & symbol_name = typeid(T_ComponentTest).name();
+	std::ostringstream componentTagName;
+	componentTagName
+		<< std::left
+		<< std::setw(45)
+		<< symbol_name
+		;
+
 	try
 	{
-		const std::string & symbol_name = typeid(T_ComponentTest).name();
-		std::cout << "[Test] : [" << std::setw(30) << symbol_name << "] : Processing ... " << std::endl;
+		std::cout << "[Test] : [" << componentTagName.str() << "] : Processing ... " << std::endl;
 
 		using T_ClockSystem = std::chrono::high_resolution_clock;
 
@@ -29,14 +37,14 @@ bool	TestComponent(void)
 		bool ret = T_ComponentTest::Proceed();
 		const long long elasped_usec = std::chrono::duration_cast<std::chrono::microseconds>(T_ClockSystem::now() - tp_start).count();
 
-		std::cout << "[Test] : [" << std::setw(30) << symbol_name << "] : .............. : [" << (ret ? "PASSED" : "FAILED") << "] in " << elasped_usec  << "ms" << std::endl << std::endl;
+		std::cout << "[Test] : [" << componentTagName.str() << "] : .............. : [" << (ret ? "PASSED" : "FAILED") << "] in " << elasped_usec  << "ms" << std::endl << std::endl;
 
 		return ret;
 	}
 	catch (const std::exception & ex)
 	{
 		std::cerr
-			<< "[Error]::[Test] :  [" << std::setw(30) << typeid(T_ComponentTest).name() << "] : ..... FAILED" << std::endl
+			<< "[Error]::[Test] :  [" << componentTagName.str() << "] : ..... FAILED" << std::endl
 			<< "|- std::exception catch : [" << ex.what() << "]" << std::endl
 			<< std::endl
 			;
@@ -45,7 +53,7 @@ bool	TestComponent(void)
 	catch (...)
 	{
 		std::cerr
-			<< "[Error]::[Test] : [" << std::setw(30) << typeid(T_ComponentTest).name() << "] : ..... FAILED" << std::endl
+			<< "[Error]::[Test] : [" << componentTagName.str() << "] : ..... FAILED" << std::endl
 			<< "|- Unknown type catch" << std::endl
 			<< std::endl
 			;
