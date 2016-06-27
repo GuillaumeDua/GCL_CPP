20af5a1e417decf11a0afb78b93d328d2115f7ec
Guss <guillaume.dua@gmail.com>
2016-06-27 14:34:39 +0200

    Update TestUtils.h
    Makes it thread safe, reliable, and increase controlability

--- GCL_CPP/TestUtils.h
+++ GCL_CPP/TestUtils.h
@@ -10,6 +10,7 @@
 #include <string>
 #include <chrono>
 #include <thread>
+#include <mutex>
 
 namespace GCL
 {
@@ -17,108 +18,126 @@ namespace GCL
 	{
 		namespace TestUtils
 		{
-
-			struct RT_Exception_controler
+			namespace Inline
 			{
-				struct Observer
+				struct RT_Exception_controler final
 				{
-					virtual void	NotifyPropertyChanged(void) = 0;
+					using key_type = std::string;
+					using mapped_type = bool;
+					using value_type = std::pair<const key_type, mapped_type>;
+					
+					struct Observer
+					{
+						virtual void	NotifyPropertyChanged(value_type &) = 0;
+					};
+					using T_ObserversList = std::list<std::shared_ptr<Observer>>;
+
+				private:
+					using T_Container = std::unordered_map<key_type, mapped_type>;
+					static T_Container		_container;
+					static std::mutex		_container_mutex;
+					static T_ObserversList	_observers;
+					static std::mutex		_obs_mutex;
+					static volatile bool	_isActive;
+					static void								OnPropertyChanged(value_type & property)
+					{
+						std::async(std::launch::async, [&property]()
+						{
+							std::lock_guard<std::mutex> lock(_obs_mutex);
+							for (auto & obs : _observers)
+								obs->NotifyPropertyChanged(property);
+						});
+					}
+
+				public:
+
+					static inline volatile bool &			IsActive(void)
+					{
+						return _isActive;
+					}
+					static void								AddObserver(std::shared_ptr<Observer> & obs)
+					{
+						std::lock_guard<std::mutex> lock(_obs_mutex);
+						_observers.push_back(obs);
+					}
+					static T_Container::mapped_type			Get(const T_Container::key_type & key)
+					{
+						std::lock_guard<std::mutex>	lock(_container_mutex);
+						T_Container::iterator it = _container.find(key);
+						if (it == _container.end())
+						{
+							auto insert_ret = _container.emplace(value_type(key, false));
+							assert(insert_ret.second);
+							OnPropertyChanged(*(insert_ret.first));	// costly
+						}
+						return _container.at(key);
+					}
+
+					static inline void						Activate(const size_t distance)
+					{
+						std::lock_guard<std::mutex>	lock(_container_mutex);
+						T_Container::iterator it = _container.begin();
+						std::advance(it, distance);
+						it->second = true;
+						std::cout << "[+]::[INFO] : Activating hookpoint at : " << it->first << std::endl;
+					}
+					static inline void						Activate(const key_type key)
+					{
+						std::lock_guard<std::mutex>	lock(_container_mutex);
+						T_Container::iterator it = _container.find(key);
+						assert(it != _container.end());
+						it->second = true;
+					}
 				};
 
-				using T_KeyHash = std::string;	// todo : key_type, mapped_type, iterator : STL compatibility
-				struct T_HookPoint
+#define CONDITIONAL_SCOPE_INLINE(expr)		static const GCL::Experimental::TestUtils::Inline::RT_Exception_controler::key_type key = std::string(__FILE__ " line ") + std::to_string(__LINE__);													\
+											GCL::Experimental::TestUtils::Inline::RT_Exception_controler::mapped_type hook_active = RT_Exception_controler::IsActive() && GCL::Experimental::TestUtils::Inline::RT_Exception_controler::Get(key);	\
+											if (hook_active)																																														\
+											{																																																		\
+												expr																																																\
+											}
+
+				struct Test
 				{
-					using T_Callback = std::function<void()>;
+					struct LogObserver : RT_Exception_controler::Observer
+					{
+						void	NotifyPropertyChanged(RT_Exception_controler::value_type & elem) override
+						{
+							std::cout << "[+]::[TEST] : New entry at : " << elem.first << std::endl;
+							elem.second = true;	// activate the element
+							std::cout << "[+]::[TEST] : New entry activated" << std::endl;
+						}
+					};
 
-					T_HookPoint(T_Callback && cb)
-						: _conditionnalSnippet(cb)
-					{}
+					static bool Proceed(void)
+					{
+						std::shared_ptr<RT_Exception_controler::Observer> logObs = std::make_shared<LogObserver>();
 
-					T_Callback	_conditionnalSnippet = nullptr;
-					bool isActive = false;
-					// todo : std::string description : callback code to string
+						RT_Exception_controler::AddObserver(logObs);
+
+						/*RT_Exception_controler::IsActive() = true;*/
+
+						for (size_t i = 0; i < 2; ++i)
+						{
+							try
+							{
+								CONDITIONAL_SCOPE_INLINE(throw std::runtime_error("std::runtime_error"););
+								std::cout << "[+]::[TEST] : " << i << " : No throw" << std::endl;
+							}
+							catch (const std::exception & ex)
+							{
+								std::cout << "[+]::[TEST] : " << i << " : Exception catch : " << ex.what() << std::endl;
+							}
+
+							// RT_Exception_controler::Activate(0);					// simulate activation at index 0
+						}
+						return true;
+					}
 				};
-				using T_ObserversList = std::list<std::shared_ptr<Observer>>;
-
-			private:
-				using T_Container = std::unordered_map<T_KeyHash, T_HookPoint>;
-				static T_Container _container;
-				static T_ObserversList	_observers;
-
-			public:
-
-				static void								OnPropertyChanged(void)
-				{
-					for (auto & obs : _observers)
-						obs->NotifyPropertyChanged();
-				}
-				static const T_Container::mapped_type &	Insert(T_Container::value_type && value)
-				{
-					T_Container::iterator it = _container.find(value.first);
-					if (it == _container.end())
-					{
-						auto insert_ret = _container.emplace(value);
-						assert(insert_ret.second);
-						it = insert_ret.first;
-						OnPropertyChanged();
-					}
-					return it->second;
-				}
-
-				static inline void	Activate(const size_t distance)
-				{
-					T_Container::iterator it = _container.begin();
-					std::advance(it, distance);
-					Activate(it);
-				}
-				static inline void	Activate(const T_KeyHash key)
-				{
-					Activate(_container.find(key));
-				}
-				static inline void	Activate(T_Container::iterator & it)
-				{
-					it->second.isActive = true;
-					std::cout << "[+]::[INFO] : Activating hookpoint at : " << it->first << std::endl;
-				}
-			};
-
-#define CONDITIONAL_SNIPPET(expr)	static const GCL::Experimental::TestUtils::RT_Exception_controler::T_KeyHash key = std::string(__FILE__ " line ") + std::to_string(__LINE__);																																										\
-									const GCL::Experimental::TestUtils::RT_Exception_controler::T_HookPoint & hook =  GCL::Experimental::TestUtils::RT_Exception_controler::Insert(std::pair<GCL::Experimental::TestUtils::RT_Exception_controler::T_KeyHash, GCL::Experimental::TestUtils::RT_Exception_controler::T_HookPoint>(key, [&]() { expr }));	\
-									if (hook.isActive)																																																																									\
-										hook._conditionnalSnippet();
-			struct Test
-			{
-				static bool Proceed(void)
-				{
-					for (size_t i = 0; i < 2; ++i)
-					{
-						std::cout << "[+]::[TEST] : " << i << " : ";
-						try
-						{
-							CONDITIONAL_SNIPPET(throw std::runtime_error("std::runtime_error"););
-							std::cout << "No throw" << std::endl;
-						}
-						catch (const std::exception & ex)
-						{
-							std::cout << "Exception catch : " << ex.what() << std::endl;
-						}
-						std::this_thread::sleep_for(std::chrono::seconds(1));
-
-						RT_Exception_controler::Activate(0);					// simulate activation at index 0
-					}
-					return true;
-				}
-			};
+			}
 		}
 	}
 }
-//#define CONDITIONAL_SNIPPET(expr)	static const RT_Exception_controler::T_KeyHash key = std::string(__FILE__ " line ") + std::to_string(__LINE__);	\
-//									RT_Exception_controler::T_Container::iterator it = RT_Exception_controler::_container.find(key);				\
-//									if (it == RT_Exception_controler::_container.cend())															\
-//									RT_Exception_controler::_container.insert(std::make_pair(key, std::move([&]() { expr })));						\
-//									else if (it->second.isActive)																					\
-//									it->second._conditionnalSnippet();
-
 
 
 #endif // GCL_TEST_UTILS__
