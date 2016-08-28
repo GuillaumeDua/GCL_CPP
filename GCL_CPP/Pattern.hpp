d8203073e75828ef84f373a5449eef8f507ca013
GuillaumeDua <guillaume.dua@gmail.com>
2016-08-28 19:26:44 +0200

    Fix issue #8

--- GCL_CPP/Pattern.hpp
+++ GCL_CPP/Pattern.hpp
@@ -6,6 +6,9 @@
 # include <thread>
 # include <future>
 # include <chrono>
+# include <memory>
+# include <type_traits>
+# include <utility>
 
 namespace GCL
 {
@@ -13,75 +16,99 @@ namespace GCL
 	{
 		namespace Pattern
 		{
-			namespace Module
+			namespace Module // Filter
 			{
 				template <typename T_ToVisitType>
 				struct Type
 				{
-					virtual void	Visit(T_ToVisitType &) = 0;
+					virtual void	Visit(T_ToVisitType) = 0;
 				};
-				template <typename T_ToVisitType>
-				struct ChainElementFilter
+				template <typename T_ToVisitElement>
+				struct Filter
 				{
-					using T_ToVisitElement = typename T_ToVisitType::T_Element;
+					using _ToVisitElement = T_ToVisitElement;
+					virtual void	Visit(_ToVisitElement) = 0;
+				};
+				template <typename T_ToVisitElement>
+				struct FilterChain
+				{
+					using T_Filter = Filter<T_ToVisitElement>;
+					using T_FilterList = std::list<std::shared_ptr<T_Filter>>;
+					using _ToVisitElement = typename T_Filter::_ToVisitElement;
 
-					virtual void	Visit(T_ToVisitElement &) = 0;
+					void	Process(T_ToVisitElement & elem)
+					{
+						for (auto & filter : _filters)
+							; // (*filter)(elem); // fixme
+					}
+
+					T_FilterList	_filters;
 				};
 			}
 			// Todo : Runnable class with status : Starting, Started, Running, Stoping, Stopped
 			template <typename T_ElementType>
 			struct ControlCenter final
 			{
-				using T_Element = T_ElementType;
-				using T_ThisType = ControlCenter<T_Element>;
+				RELEASE_INSTRUCTION
+				(static_assert(std::is_constructible<T_ElementType>::value == false, "Optimization error : T_ElementType cannot be constructible");)
 
-				using T_FilterType = Module::ChainElementFilter<T_ThisType>;
-				using T_FilterList = std::list<std::shared_ptr<T_FilterType>>;
-				using T_ElementQueue = std::queue<T_Element>;
+				using T_ThisType = ControlCenter<T_ElementType>;
+				using T_Element			= std::unique_ptr<T_ElementType>;
+				using T_FilterChain		= Module::FilterChain<T_Element>;
+				using T_ElementQueue	= typename std::queue<T_Element>;
+				static_assert(std::is_same<typename T_FilterChain::_ToVisitElement, typename T_ElementQueue::value_type>::value, "Type mismatch");
 
-				static void	Push(T_Element && rEvent)
-				{
-					T_ThisType & instance = GetInstance();
-					{	// Critical section
-						std::unique_lock<std::mutex> lock(instance._mutex);
-						instance._cache.push(rEvent);
-					}
-					instance._cv.notify_one();
-				}
-				static inline T_ThisType & GetInstance(void)
+				ControlCenter() = default;
+
+				static inline		T_ThisType & GetInstance(void)
 				{
 					static T_ThisType instance;
 					return instance;
 				}
-
-				DEBUG_INSTRUCTION
-				(static void inline EmergencyCacheFlush(void)
+				static void			Push(T_Element && rEvent)
 				{
-					ControlCenter & instance = GetInstance();
-					std::unique_lock<std::mutex> lock(instance._mutex);
-					instance._cache.swap(T_ElementQueue());
+					T_ThisType & instance = GetInstance();
+					instance._Push(std::move(rEvent)); // fixme
+				}
+				void				_Push(T_Element && rEvent)
+				{
+					{	// Critical section
+						std::unique_lock<std::mutex> lock(_mutex);
+						_cache.push(std::move(rEvent));
+					}
+					_cv.notify_one();
+				}
+				DEBUG_INSTRUCTION
+				(void inline		_EmergencyCacheFlush(void)
+				{
+					std::unique_lock<std::mutex> lock(_mutex);
+					_cache.swap(T_ElementQueue());
+				})
+				DEBUG_INSTRUCTION
+				(static void inline	EmergencyCacheFlush(void)
+				{
+					T_ThisType & instance = GetInstance();
+					instance._EmergencyCacheFlush();
 				})
 
-				void Start(void)
+				void				Start(void)
 				{
 					_running = true;
-
 					if (_thread != nullptr)
 						throw std::runtime_error("Event::ControlCenter::Start : Already started");
-
 					_thread.reset();
 
 					struct CaptureAdapter
 					{
-						std::mutex & _mutex;
+						std::mutex				& _mutex;
 						std::condition_variable & _cv;
-						T_ElementQueue &_cache;
-						T_FilterList & _filters;
-					} capture { _mutex, _cv, _cache, _filters };
+						T_ElementQueue			&_cache;
+						T_FilterChain			& _filterChain;
+					} capture { _mutex, _cv, _cache, _filterChain };
 
 					_thread = std::make_unique<std::thread>([&](CaptureAdapter & capture)
 					{
-						T_ElementQueue::value_type elem;
+						T_ElementQueue::value_type elem = 0x0;
 						while (this->IsRunning())
 						{
 							if (capture._cache.empty())
@@ -97,42 +124,36 @@ namespace GCL
 									capture._cache.pop();
 								}
 
-								for (auto & filter : capture._filters)
-									filter->Visit(elem);
-								elem();
+								capture._filterChain.Process(elem);
+
+								(*elem)();
 							}
 						}
 					}, capture);
 				}
-				void Stop(void)
+				void				Stop(void)
 				{
 					_running = false;
 					if (_thread != nullptr)
 					{
-						GetInstance()._cv.notify_all();
+						// GetInstance()._cv.notify_all();
+						this->_cv.notify_all();
 						_thread->join();
 					}
 					_thread.reset();
 				}
-				inline bool	IsRunning(void) const
+				inline bool			IsRunning(void) const
 				{
 					return _running;
 				}
-				inline const size_t Pending(void) const
+				inline const size_t	Pending(void) const
 				{
 					return _cache.size();
 				}
 
 			protected:
-				T_FilterList	_filters;
+				T_FilterChain	_filterChain;
 				T_ElementQueue	_cache;
-
-				void ProcessElement(T_Element & elem)
-				{
-					for (auto & filter : _filters)
-						filter->Visit(elem);
-					elem();
-				}
 
 			private:
 				std::unique_ptr<std::thread>	_thread;
@@ -140,16 +161,16 @@ namespace GCL
 				std::condition_variable			_cv;
 				volatile bool					_running = false;
 
-				ControlCenter() = default;
-				ControlCenter(const ControlCenter &) = default;
-				ControlCenter(ControlCenter &&) = delete;
+				ControlCenter(const ControlCenter &)	= default;
+				ControlCenter(ControlCenter &&)			= delete;
 			};
 
 			struct Test
 			{
 				struct Event final
 				{
-					explicit Event() = default;
+					Event() = default;
+					Event(Event &&) = default;
 					void operator()(void)
 					{}
 				};
@@ -172,13 +193,13 @@ namespace GCL
 						static const long long qty = std::chrono::microseconds::period::den;
 						std::cout << "\t |- Generating " << qty << " elements ..." << std::endl;
 						for (long long i = 0; i < qty; ++i)
-							ControlCenterType::Push(Event{});
+							ControlCenterType::Push(std::move(std::make_unique<Event>()));
 
 						std::cout << "\t |- Racing ..." << std::endl;
 						ControlCenterType::GetInstance().Start();
 						std::this_thread::sleep_for(std::chrono::seconds(1));
 						ControlCenterType::GetInstance().Stop();
-						std::cout << "\t |- Processed elements : " << (qty - ControlCenterType::GetInstance().Pending()) << " in 1 second" << std::endl;
+						std::cout << "\t |- Processed elements : " << (qty - ControlCenterType::GetInstance().Pending()) << " in 1 second" << std::endl; // [+ 200.000, 215.000]
 					}
 
 					ControlCenterType::EmergencyCacheFlush();
@@ -190,7 +211,7 @@ namespace GCL
 						for (long long i = 0; i < qty; ++i)
 						{
 							std::this_thread::sleep_for(std::chrono::milliseconds(1));
-							ControlCenterType::Push(Event{});
+							ControlCenterType::Push(std::move(std::make_unique<Event>()));
 						}
 						ControlCenterType::GetInstance().Stop();
 						std::cout << "\t |- Processed elements : " << (qty - ControlCenterType::GetInstance().Pending()) << " / " << qty << std::endl;
