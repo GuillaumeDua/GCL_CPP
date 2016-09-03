2f62abc876958e7cfe5fd9cdc41b69555dc029fe
GuillaumeDua <guillaume.dua@gmail.com>
2016-09-03 13:22:20 +0200

    TypeTrait file repshaping : divide SFINAE and classic code

--- GCL_CPP/TypeTraits.h
+++ GCL_CPP/TypeTraits.h
@@ -1,10 +1,13 @@
 #ifndef GCL_TRAITS__
 # define GCL_TRAITS__
 
+# include "TypeTrait_SFINAE.hpp"
+
 namespace GCL
 {
 	namespace TypeTrait
 	{
+		// Warning : Values change per program instance
 		template <typename T>
 		struct	TypeToUniqueId
 		{
@@ -43,83 +46,18 @@ namespace GCL
 
 		template <class T, class Tuple>
 		struct IndexOf;
-
 		template <class T, class... _Types>
 		struct IndexOf<T, std::tuple<T, _Types...> >
 		{
 			static const std::size_t value = 0;
 		};
-
 		template <class T, class U, class... _Types>
 		struct IndexOf<T, std::tuple<U, _Types...> >
 		{
 			static const std::size_t value = 1 + IndexOf<T, std::tuple<_Types...> >::value;	// Todo : Check recusion
 		};
 
-		// Substitute to C++17 feature : constraint/require
-		// Allow sfinae static type checking in C++TMP
-#pragma region Waiting_for_Cpp17_constraint_require_reflexion
-		namespace sfinae
-		{
-		    template<typename> struct Gen { typedef void type; };
-		}
-		#define gen_has_nested_type(name)                                   \
-		namespace GCL                                                       \
-		{                                                                   \
-		    namespace TypeTrait                                             \
-		    {                                                               \
-		        namespace sfinae                                            \
-		        {                                                           \
-		            template<typename T, typename SFINAE = void>            \
-		            struct HasNestedType_##name : std::false_type {};       \
-		                                                                    \
-		            template<typename T>                                    \
-		            struct HasNestedType_##name                             \
-		            <                                                       \
-		                    T                                               \
-		                ,   typename Gen                                    \
-		                    <                                               \
-		                        typename T::NestedType                      \
-		                    >::type                                         \
-		            > : std::true_type {};                                  \
-		                                                                    \
-		            template <class T>                                      \
-		            constexpr inline bool has_nested_type_##name(void)      \
-		            {                                                       \
-		                return std::is_same<typename HasNestedType_##name<T>::type, std::true_type>::value; \
-		            }                                                       \
-		        }                                                           \
-		    }                                                               \
-		}
-		#define gen_has_member_function(name)                               \
-		namespace GCL                                                       \
-		{                                                                   \
-		    namespace TypeTrait                                             \
-		    {                                                               \
-		        namespace sfinae                                            \
-		        {                                                           \
-		            template<typename T, typename SFINAE = void>            \
-		            struct HasMemberFunction##name : std::false_type {};    \
-		                                                                    \
-		            template<typename T>                                    \
-		            struct HasMemberFunction##name                          \
-		            <                                                       \
-		                    T                                               \
-		                ,   typename Gen                                    \
-		                    <                                               \
-		                        decltype(&T:: name)                         \
-		                    >::type                                         \
-		            > : std::true_type {};                                  \
-		                                                                    \
-		            template <class T>                                      \
-		            constexpr inline bool has_member_function_##name(void)  \
-		            {                                                       \
-		                return std::is_same<typename HasMemberFunction##name<T>::type, std::true_type>::value; \
-		            }                                                       \
-		        }                                                           \
-		    }                                                               \
-		}
-#pragma endregion Waiting_for_Cpp17_constraint_require_reflexion
+
 
 		struct Test
 		{
