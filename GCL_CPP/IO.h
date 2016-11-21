bdcc820d7edb8be5bc682a2cd912d1f671e4da36
GuillaumeDua <guillaume.dua@gmail.com>
2016-11-21 02:53:36 +0100

    Adding IO::Stream

--- GCL_CPP/IO.h
+++ GCL_CPP/IO.h
@@ -4,6 +4,8 @@
 # include <iostream>
 # include "Preprocessor.h"
 
+# include <sstream> // For test purpose only
+
 namespace GCL
 {
 	namespace IO
@@ -55,6 +57,42 @@ namespace GCL
 					_GCL_ASSERT(os);
 					os.write(&var[0], var.length());
 					_GCL_ASSERT(os);
+				}
+			};
+			struct Stream
+			{
+				template <typename T>
+				static void	write(std::ostream & os, const T & var)
+				{
+					os << var;
+					_GCL_ASSERT(os);
+				}
+				template <typename T>
+				static void	read(std::istream & is, T & var)
+				{
+					is >> var;
+					_GCL_ASSERT(is);
+				}
+				template <typename T>
+				static void	write(std::ostream & os, const T * var)
+				{
+					Binary::write<T>(os, var);
+				}
+				template <typename T>
+				static void	read(std::istream & is, T * var)
+				{
+					Binary::read<T>(is, var);
+				}
+
+				template <>
+				static void read<std::string>(std::istream & is, std::string & var)
+				{
+					Binary::read<std::string>(is, var);
+				}
+				template <>
+				static void write<std::string>(std::ostream & os, const std::string & var)
+				{
+					Binary::write<std::string>(os, var);
 				}
 			};
 		}
