3a58c0dd7f27c443b56fe465703db86129b27a2b
GuillaumeDua <guillaume.dua@gmail.com>
2021-01-02 15:28:19 +0100

    add gcl::type_traits::is_template and is_instanciation_of

--- includes/gcl/mp/type_traits.hpp
+++ includes/gcl/mp/type_traits.hpp
@@ -4,5 +4,18 @@
 
 namespace gcl::mp::type_traits
 {
+    template <class T>
+    struct is_template : std::false_type {};
+    template <class... T_args, template <class...> class T>
+    struct is_template<T<T_args...>> : std::true_type {};
+    template <class T>
+    static inline constexpr auto is_template_v = is_template<T>::value;
 
+    template <class T_concrete, template <class...> class T>
+    struct is_instance_of : std::false_type {};
+    template <template <class...> class T, class... T_args>
+    struct is_instance_of<T<T_args...>, T> : std::true_type {};
+
+    template <class T_concrete, template <class...> class T>
+    static inline constexpr auto is_instance_of_v = is_instance_of<T_concrete, T>::value;
 }
