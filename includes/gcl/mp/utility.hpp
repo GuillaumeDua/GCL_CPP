918305e251ff5530da7e47315f5c8766d94ed412
GuillaumeDua <guillaume.dua@gmail.com>
2021-02-07 22:12:58 +0100

    [gcl::utility] reverse_integer_sequence, make_reverse_integer_sequence, reverse_index_sequence, make_reverse_index_sequence

---
+++ includes/gcl/mp/utility.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <utility>
+#include <type_traits>
+#include <tuple>
+
+namespace gcl::mp::utility
+{
+	template <typename T, T value>
+    constexpr static auto reverse_integer_sequence_v = []<typename T, T ... indexes>(std::integer_sequence<T, indexes...>)
+    {
+        return std::integer_sequence<T, (value - 1 - indexes)...>{};
+    }(std::make_integer_sequence<T, value>{});
+
+    template <typename T, T value>
+    using make_reverse_integer_sequence = decltype(reverse_integer_sequence_v<T, value>);
+    template <std::size_t N>
+    using make_reverse_index_sequence = make_reverse_integer_sequence<std::size_t, N>;
+    template <class ... Ts>
+    using reverse_index_sequence_for = make_reverse_index_sequence<sizeof...(Ts)>;
+
+    template <std::size_t ... Ints>
+    constexpr static auto reverse_index_sequence_v = []<std::size_t ... indexes>(std::index_sequence<indexes...>){
+        constexpr auto Ints_as_tuple = std::tuple{Ints...};
+        return std::index_sequence<std::get<indexes>(Ints_as_tuple)...>{};
+    }(reverse_index_sequence_for<decltype(Ints)...>{});
+
+    template <std::size_t ... Ints>
+    using reverse_index_sequence = std::decay_t<decltype(reverse_index_sequence_v<Ints...>)>;
+}
+
+namespace gcl::mp::utility::tests
+{
+    static_assert(std::is_same_v<
+        reverse_index_sequence<11, 22, 33>,
+        std::index_sequence<33, 22, 11>
+    >);
+}
