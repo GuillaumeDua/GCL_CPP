9e2be9b2e7e670f2ea286d5fad5d24d6ae86bb77
GuillaumeDua <guillaume.dua@gmail.com>
2021-02-20 15:40:00 +0100

    [gcl::cx] add array::remove_duplicates as a bridge between mp and cx

---
+++ includes/gcl/cx/array.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <gcl/mp/type_traits.hpp>
+
+#include <array>
+#include <algorithm>
+#include <type_traits>
+#include <ranges>
+
+namespace gcl::cx::concepts
+{
+    template <typename T>
+    concept array_value = gcl::mp::type_traits::is_std_array_v<T> or std::is_array_v<T>;
+}
+
+namespace gcl::cx::array
+{
+    template <concepts::array_value auto arg>
+    consteval static auto remove_duplicates()
+    {
+        using argument_type = decltype(arg);
+        using value_type = typename argument_type::value_type;
+        constexpr auto size = []() consteval
+        {
+            auto copy = arg;
+            std::ranges::sort(copy);
+            return std::ranges::size(arg) - std::ranges::size(std::ranges::unique(copy));
+        }
+        ();
+
+        auto copy = arg;
+        std::ranges::sort(copy);
+        auto last_not_duplicate = std::unique(std::begin(copy), std::end(copy));
+
+        using return_type = std::array<value_type, size>;
+        return_type return_value;
+        std::copy(std::begin(copy), last_not_duplicate, std::begin(return_value));
+        return return_value;
+    };
+    template <concepts::array_value auto arg>
+    constexpr static auto remove_duplicates_v = remove_duplicates<arg>();
+    template <concepts::array_value auto arg>
+    using remove_duplicates_t = typename std::decay_t<decltype(remove_duplicates_v<arg>)>;
+}
+
+namespace gcl::cx::tests::array
+{
+    constexpr auto datas = std::array{'a', 'b', 'a', 'c', 'a'};
+    static_assert(std::is_same_v<std::array<char, 3>, gcl::cx::array::remove_duplicates_t<datas>>);
+    static_assert(gcl::cx::array::remove_duplicates_v<datas> == std::array{'a', 'b', 'c'});
+}
--- includes/gcl/cx/cx.hpp
+++ includes/gcl/cx/cx.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
+#include <gcl/cx/array.hpp>
 #include <gcl/cx/crc32_hash.hpp>
 #include <gcl/cx/map.hpp>
