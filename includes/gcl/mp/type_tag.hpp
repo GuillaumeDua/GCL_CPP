994e7746a7177c863ebd72e694ea932b01730b96
GuillaumeDua <guillaume.dua@gmail.com>
2021-03-13 15:22:31 +0100

    [mp::tag_type] POC

---
+++ includes/gcl/mp/type_tag.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <utility>
+#include <concepts>
+
+namespace gcl::type_tag
+{
+    template <typename tag_t>
+    concept tag_type = requires(tag_t)
+    {
+        typename tag_t::value_type;
+        {
+            std::decay_t<decltype(tag_t::value)> {}
+        }
+        ->std::same_as<typename tag_t::value_type>;
+    };
+
+    template <tag_type tag>
+    struct add_tag {
+        constexpr static typename tag::value_type get_value(typename tag::value_type) { return tag::value; }
+    };
+
+    template <tag_type... tag_arguments>
+    class add_tags : add_tag<tag_arguments>... {
+        using add_tag<tag_arguments>::get_value...;
+
+      public:
+        template <typename T>
+        constexpr static T value = get_value(T{});
+    };
+}
