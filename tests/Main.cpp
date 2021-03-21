917c3e2d3e6884546f8a63360386660a820c1bb8
GuillaumeDua <guillaume.dua@gmail.com>
2021-03-21 14:47:07 +0100

    [algorithms] add ranges::is_in_range tests RT

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -1,15 +1,14 @@
 // #define _GCL_DEBUG
 
 // static tests
-
 #include <gcl/mp/mp.hpp>
 #include <gcl/cx/cx.hpp>
 #include <gcl/compile_time_constant/ctc.hpp>
 
 // dynamic tests
-
 #include <gcl/serialization.hpp>
 #include <gcl/container/interval_map.hpp>
+#include <gcl/algorithms/algorithms.hpp>
 
 void tmp()
 {	// only for gcl_v2 WIP
@@ -28,6 +27,7 @@ auto main(int, char const*[]) -> int
         // gcl::signals::initialize();
         // gcl::test::proceed();
         gcl::container::test::interval_map::test();
+        gcl::algorithms::tests::ranges::test();
 
         std::cout << "gcl : all runtime tests succeed\n";
     }
