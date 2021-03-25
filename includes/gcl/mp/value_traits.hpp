5aac487beb4625b54fd080960671ea475aa5a71f
GuillaumeDua <guillaume.dua@gmail.com>
2021-03-25 18:51:24 +0100

    [mp/value_traits] add bit_size_v<T>

--- includes/gcl/mp/value_traits.hpp
+++ includes/gcl/mp/value_traits.hpp
@@ -27,6 +27,17 @@ namespace gcl::mp::value_traits
     template <auto... values>
     constexpr static auto not_equal_v = not equal_v<values...>;
 }
+#include <cstdint>
+namespace gcl::mp::value_traits
+{
+    template <typename T>
+    constexpr std::size_t bit_size_v = sizeof(T) * CHAR_BIT;
+    // sizeof(bool) is implementation-defined,
+    // but here we expect - thus, define - that a boolean is 1 bit-long
+    template <>
+    constexpr std::size_t bit_size_v<bool> = 1;
+}
+
 
 namespace gcl::mp::value_traits::tests::equal
 {
@@ -46,4 +57,25 @@ namespace gcl::mp::value_traits::tests::equal
     static_assert(not_equal_v<true, false, true, true>);
     static_assert(not_equal_v<true, true, false, true>);
     static_assert(not_equal_v<true, true, true, false>);
+}
+
+#include <gcl/mp/system_info.hpp>
+namespace gcl::mp::value_traits::tests
+{
+    static void bit_size_v()
+    {
+        if constexpr (gcl::mp::system_info::is_x64)
+        {   // might be wrong, depending on the target/plateform
+            static_assert(gcl::mp::value_traits::bit_size_v<bool> == 1);
+            static_assert(gcl::mp::value_traits::bit_size_v<char> == 8);
+            static_assert(gcl::mp::value_traits::bit_size_v<int8_t> == 8);
+            static_assert(gcl::mp::value_traits::bit_size_v<uint8_t> == 8);
+            static_assert(gcl::mp::value_traits::bit_size_v<int16_t> == 16);
+            static_assert(gcl::mp::value_traits::bit_size_v<uint16_t> == 16);
+            static_assert(gcl::mp::value_traits::bit_size_v<int32_t> == 32);
+            static_assert(gcl::mp::value_traits::bit_size_v<uint32_t> == 32);
+            static_assert(gcl::mp::value_traits::bit_size_v<int64_t> == 64);
+            static_assert(gcl::mp::value_traits::bit_size_v<uint64_t> == 64);
+        }
+    }
 }
