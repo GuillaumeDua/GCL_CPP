bec742b1f64d814a3cdde9475de7dd4bddfc3c58
GuillaumeDua <guillaume.dua@gmail.com>
2021-03-25 18:12:06 +0100

    [cx::hash_type] add alias for convinience

--- includes/gcl/cx/crc32_hash.hpp
+++ includes/gcl/cx/crc32_hash.hpp
@@ -5,18 +5,19 @@
 
 namespace gcl::cx
 {
+    using hash_type = uint32_t;
     struct crc_32
     {
-        constexpr static uint32_t hash(std::string_view str)
+        constexpr static hash_type hash(std::string_view str)
         {
-            uint32_t crc = 0xffffffff;
+            hash_type crc = 0xffffffff;
             for (auto c : str)
                 crc = (crc >> 8) ^ crc_table[(crc ^ c) & 0xff];
             return crc ^ 0xffffffff;
         }
 
       private:
-        static constexpr uint32_t crc_table[256] = {
+        static constexpr hash_type crc_table[256] = {
             0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3, 0x0edb8832,
             0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, 0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
             0xf3b97148, 0x84be41de, 0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7, 0x136c9856, 0x646ba8c0, 0xfd62f97a,
