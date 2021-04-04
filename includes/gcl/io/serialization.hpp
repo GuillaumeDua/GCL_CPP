862213460e36922cdffdd89e11214b3caabba733
GuillaumeDua <guillaume.dua@gmail.com>
2021-04-04 15:46:55 +0200

    [io::engine] overload pattern handler -> differ function call using serialization (POC OK, need cleanup)

--- includes/gcl/io/serialization.hpp
+++ includes/gcl/io/serialization.hpp
@@ -16,8 +16,22 @@
 
 namespace gcl::io::serialization
 {
+    template <typename... Ts>
+    struct signature {
+        using storage_type = std::tuple<Ts...>;
+        storage_type storage;
+
+        constexpr signature(Ts&&... args)
+            : storage{std::forward<Ts>(args)...}
+        {}
+        constexpr signature(const Ts&... args)
+            : storage{std::forward<Ts>(args)...}
+        {}
+    };
+
     template <class io_policy = gcl::io::policy::binary>
     struct engine {
+
         template <typename on_deserialization_t>
         class in {
             using type_id_t = uint32_t;
@@ -48,21 +62,41 @@ namespace gcl::io::serialization
             {
                 if constexpr (gcl::functional::type_traits::is_overload_v<on_deserialization_t>)
                 {
-                    auto register_each_tuple_element = [this]<typename... Ts>(std::tuple<Ts...>)
+                    const auto register_signature = [this]<typename... Ts>(std::tuple<Ts...>)
                     {
                         static_assert(((not std::is_const_v<Ts>)&&...));
                         static_assert(((not std::is_reference_v<Ts>)&&...));
 
-                        register_types<Ts...>();
+                        using handler_signature = signature<Ts...>;
+                        storage.insert(
+                            std::pair{gcl::cx::typeinfo::hashcode<handler_signature>(), std::function<void()>{[this]() {
+                                          std::tuple<Ts...> arguments;
+                                          const auto        read_one_argument = [this]<typename T>(T&& value) {
+
+                                              io_policy::read(input_stream, std::forward<T>(value));
+                                              if (not input_stream.good())
+                                                  throw std::runtime_error{
+                                                      "serialization::engine::deserialize (value) / "
+                                                      "storage::mapped::operator() "};
+                                          };
+                                          [ this, &arguments, &
+                                            read_one_argument ]<std::size_t... indexes>(std::index_sequence<indexes...>)
+                                          {
+                                              ((read_one_argument(std::move(std::get<indexes>(arguments)))), ...);
+                                          }
+                                          (std::make_index_sequence<std::tuple_size_v<decltype(arguments)>>{});
+
+                                          std::apply(on_deserialize, std::move(arguments));
+                                      }}});
                     };
-                    [register_each_tuple_element]<typename... Ts>(gcl::functional::overload<Ts...> &&)
+                    [register_signature]<typename... Ts>(gcl::functional::overload<Ts...> &&)
                     {
                         using remove_cv_and_ref = gcl::mp::type_traits::merge_traits<std::remove_reference_t, std::decay_t>;
 
-                        ((register_each_tuple_element(gcl::mp::type_traits::transform_t<
+                        ((register_signature(gcl::mp::type_traits::transform_t<
                             gcl::mp::function_traits_t<decltype(&Ts::operator())>::template arguments,
                             remove_cv_and_ref::type>{})), ...);
-                        
+
                     }(std::forward<decltype(cb)>(cb));
                 }
             }
@@ -77,7 +111,11 @@ namespace gcl::io::serialization
             template <gcl::io::concepts::serializable ... Ts>
             void register_types()
             {
-                (storage.insert(generate_type_handler<Ts>()), ...);
+                const auto insert_if_not_exists = [this]<typename T>() {
+                    if (not storage.contains(gcl::cx::typeinfo::hashcode_v<T>))
+                        storage.insert(generate_type_handler<T>());
+                };
+                ((insert_if_not_exists.template operator()<Ts>()), ...);
             }
             template <typename... Ts>
             void unregister_types()
@@ -150,6 +188,25 @@ namespace gcl::io::serialization
                 io_policy::write(output_stream, std::forward<T>(value));
             }
             template <typename T>
+            void serialize(const T& value) const
+            {
+                io_policy::write(output_stream, gcl::cx::typeinfo::hashcode<T>());
+                io_policy::write(output_stream, std::forward<T>(value));
+            }
+            template <typename ...Ts>
+            void serialize(signature<Ts...>&& argument) const
+            {
+                static_assert(((not std::is_reference_v<Ts>)&&...));
+                static_assert(((not std::is_const_v<Ts>)&&...));
+
+                using argument_storage_type = std::decay_t<std::remove_reference_t<decltype(argument)>>::storage_type;
+
+                io_policy::write(output_stream, gcl::cx::typeinfo::hashcode<signature<Ts...>>());
+                [&]<std::size_t... indexes>(std::index_sequence<indexes...>){
+                    ((io_policy::write(output_stream, std::move(std::get<indexes>(argument.storage)))), ...);
+                }(std::make_index_sequence<std::tuple_size_v<argument_storage_type>>{});
+            }
+            template <typename T>
             const out& operator<<(T&& value) const
             {
                 serialize(std::forward<decltype(value)>(value));
@@ -180,7 +237,9 @@ namespace gcl::io::tests::serialization
             using io_engine = typename gcl::io::serialization::engine<gcl::io::policy::binary>;
             {
                 auto serializer = io_engine::out{ss};
-                serializer << event_1{} << event_2{} << event_3{};
+                // serializer << event_1{} << event_2{} << event_3{};
+                serializer << gcl::io::serialization::signature{event_1{}, event_2{}, event_3{}};
+                serializer << gcl::io::serialization::signature{42, 'c'};
             }
             {
                 using types = std::tuple<event_1, event_2, event_3>;
@@ -189,35 +248,29 @@ namespace gcl::io::tests::serialization
                 auto deserializer = io_engine::in{
                     ss,
                     gcl::functional::overload{
-                        [&call_counter](event_1&&) mutable {
-                            if (++call_counter != 1)
-                                throw std::runtime_error{"gcl::io::tests::serialization::test : event_1"};
+                        [&call_counter](event_1&&, event_2&&, event_3&&) mutable {
+                            if (++call_counter not_eq 1)
+                                throw std::runtime_error{
+                                    "gcl::io::tests::serialization : overload signature : event : call order"};
                         },
-                        [&call_counter](event_2&&) mutable {
-                            if (++call_counter != 2)
-                                throw std::runtime_error{"gcl::io::tests::serialization::test : event_2"};
+                        [&call_counter](int&& i, char&& c) mutable {
+                            if (++call_counter not_eq 2)
+                                throw std::runtime_error{
+                                    "gcl::io::tests::serialization : overload signature : pods : call order"};
+                            if (i not_eq 42)
+                                throw std::runtime_error{
+                                    "gcl::io::tests::serialization : overload signature : pods : arg 1"};
+                            if (c not_eq 'c')
+                                throw std::runtime_error{
+                                    "gcl::io::tests::serialization : overload signature : pods : arg 2"};
                         },
-                        [&call_counter](event_3&&) mutable {
-                            if (++call_counter != 3)
-                                throw std::runtime_error{"gcl::io::tests::serialization::test : event_3"};
-                        },
-                        /*[&call_counter]<typename T>(T&& arg) mutable {
-                            static_assert(not std::is_same_v<decltype(arg), event_3>);
-
-                            if constexpr (std::is_same_v<T, event_1>)
-                                if (++call_counter not_eq 1)
-                                    throw std::runtime_error{"gcl::io::tests::serialization::test : event_1"};
-                            if constexpr (std::is_same_v<T, event_2>)
-                                if (++call_counter not_eq 2)
-                                    throw std::runtime_error{"gcl::io::tests::serialization::test : event_2"};
-                        }*/}
-                };
+                    }};
                 deserializer.deserialize_all();
             }
         }
-        catch (const std::exception& error)
+        catch (const std::exception&)
         {
-            std::cerr << "error : " << error.what() << '\n';
+            throw;
         }
     }
 }
