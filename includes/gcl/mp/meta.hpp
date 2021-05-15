97bafbeb2a8e24ef6ed31ef7eee897c3f3314d66
GuillaumeDua <guillaume.dua@gmail.com>
2021-05-15 15:42:07 +0200

    [mp::meta] : add type_sequence

--- includes/gcl/mp/meta.hpp
+++ includes/gcl/mp/meta.hpp
@@ -17,9 +17,31 @@ namespace gcl::mp::meta
     struct join<Type<Ts...>, Type<Us...>> {
         using type = Type<Ts..., Us...>;
     };
-
     template <typename... Ts>
     using join_t = typename join<Ts...>::type;
+
+    template <typename... Ts>
+    class type_sequence {
+
+        template <typename T>
+        struct flatten {
+            using type = type_sequence<T>;
+        };
+        template <typename... argument_types>
+        struct flatten<type_sequence<argument_types...>> {
+            using type = type_sequence<argument_types...>;
+        };
+        template <typename T>
+        using flatten_t = typename flatten<T>::type;
+
+        // todo : remove<types...>
+        // todo : remove_if<type_trait_as_predicate>
+        // todo : `- remove_duplicates
+
+      public:
+        template <typename... arguments_t>
+        using add = join_t<type_sequence<Ts...>, flatten_t<arguments_t>...>;
+    };
 }
 
 namespace gcl::mp::tests::meta
@@ -28,11 +50,31 @@ namespace gcl::mp::tests::meta
     struct type_seq {};
 
     using namespace gcl::mp::meta;
-    static_assert(std::is_same_v<type_seq<int, char, double>, join_t<type_seq<int>, type_seq<char, double>>>);
-    static_assert(std::is_same_v<type_seq<int, char, double>, join_t<type_seq<int, char, double>>>);
-    static_assert(
-        std::is_same_v<type_seq<int, char, double>, join_t<type_seq<int>, type_seq<char>, type_seq<double>>>);
-    static_assert(std::is_same_v<
-                  type_seq<int, char, double, float>,
-                  join_t<type_seq<int>, type_seq<char>, type_seq<double>, type_seq<float>>>);
+
+    namespace join
+    {
+        static_assert(std::is_same_v<type_seq<int, char, double>, join_t<type_seq<int>, type_seq<char, double>>>);
+        static_assert(std::is_same_v<type_seq<int, char, double>, join_t<type_seq<int, char, double>>>);
+        static_assert(
+            std::is_same_v<type_seq<int, char, double>, join_t<type_seq<int>, type_seq<char>, type_seq<double>>>);
+        static_assert(std::is_same_v<
+                      type_seq<int, char, double, float>,
+                      join_t<type_seq<int>, type_seq<char>, type_seq<double>, type_seq<float>>>);
+    }
+    namespace type_sequence_t
+    {
+        static_assert(std::is_same_v<type_sequence<int, char>, type_sequence<int>::add<char>>);
+        static_assert(std::is_same_v<type_sequence<int, char>, type_sequence<int>::add<type_sequence<char>>>);
+        static_assert(std::is_same_v<type_sequence<int, char>, type_sequence<int, char>::add<type_sequence<>>>);
+        static_assert(std::is_same_v<
+                      type_sequence<int, char, double, bool>,
+                      type_sequence<int>::add<type_sequence<>>::add<type_sequence<char>>::add<
+                          type_sequence<double>>::add<bool>>);
+        static_assert(
+            std::is_same_v<type_sequence<int, char, double>, type_sequence<int>::add<type_sequence<char>, double>>);
+        static_assert(std::is_same_v<
+                      type_sequence<int, char, double, float, bool>,
+                      type_sequence<int>::add<type_sequence<>>::add<
+                          type_sequence<char>>::add<type_sequence<double>, float>::add<bool>>);
+    }
 }
