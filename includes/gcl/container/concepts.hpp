59dc3a5ba52f9358b9f8aadf9c31290e7d345d85
GuillaumeDua <guillaume.dua@gmail.com>
2021-10-08 19:37:41 +0200

    [container::concept] : LegacyInputIterator

--- includes/gcl/container/concepts.hpp
+++ includes/gcl/container/concepts.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <utility>
+#include <concepts>
+#include <type_traits>
+#include <iterator>
+#include <utility>
 
 namespace gcl::container::concepts
 {
@@ -9,4 +13,18 @@ namespace gcl::container::concepts
     {
         {std::declval<T>().resize(std::declval<typename T::size_type>())};
     };
+
+    template<class T>
+    concept LegacyForwardIterator =
+        std::constructible_from<T> and
+        std::is_lvalue_reference_v<std::iter_reference_t<T>> and
+        std::same_as<
+            std::remove_cvref_t<std::iter_reference_t<T>>,
+            typename std::indirectly_readable_traits<T>::value_type
+        > and
+        requires(T value) {
+            {  value++ } -> std::convertible_to<const T&>;
+            { *value++ } -> std::same_as<std::iter_reference_t<T>>;
+        };
+
 }
